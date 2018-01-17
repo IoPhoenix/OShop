2202f365f426673a0e2e86a7392c6f523e7bd5f5
IoPhoenix <olga.fomin26@gmail.com>
2018-01-17 12:35:39 -0800

    Added admin role Added protected routes for admin

---
+++ src/app/admin-auth-guard.service.ts
@@ -0,0 +1,18 @@
+import { UserService } from './user.service';
+import { Injectable } from '@angular/core';
+import { CanActivate } from '@angular/router/src/interfaces';
+import { AuthService } from './auth.service';
+import 'rxjs/add/operator/switchMap';
+import { Observable } from 'rxjs/Observable';
+import { map } from 'rxjs/operator/map';
+
+@Injectable()
+export class AdminAuthGuardService implements CanActivate {
+
+  constructor(private auth: AuthService, private userService: UserService) { }
+
+  canActivate(): Observable<boolean> {
+    return this.auth.appUser$
+      .map(appUser => appUser.isAdmin);
+  }
+}
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,16 +1,23 @@
+import { UserService } from './user.service';
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase';
 import { Observable } from 'rxjs/Observable';
 import { ActivatedRoute } from '@angular/router';
+import { AppUser } from './models/app-user';
+import { switchMap } from 'rxjs/operator/switchMap';
+import 'rxjs/add/observable/of';
 
 @Injectable()
 export class AuthService {
 
   user$: Observable<firebase.User>;
   
-  constructor(private afAuth: AngularFireAuth, private route: ActivatedRoute) {
-    this.user$ = afAuth.authState;
+  constructor(
+    private userService: UserService,
+    private afAuth: AngularFireAuth, 
+    private route: ActivatedRoute) {
+      this.user$ = afAuth.authState;
    }
 
   login() {
@@ -23,5 +30,13 @@ export class AuthService {
 
   logout() {
     this.afAuth.auth.signOut();               
+  }
+
+  get appUser$() : Observable<AppUser> {
+    return this.user$
+      .switchMap(user => {
+        if (user) return this.userService.get(user.uid);
+        return Observable.of(null);
+      });
   }
 }
